8550d483ccfc1623aec16b9437c56633a618ba42
Kiraloki <lightyagami81161@gmail.com>
2023-06-27 15:04:17 +0530

    update2

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use("/api/v1/message", messageRouter);
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.use(express.static(path.join(__dirname1, "/client/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
