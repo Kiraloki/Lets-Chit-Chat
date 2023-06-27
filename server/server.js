7232dac11d620db957c3448c9fe91656a104c527
Kiraloki <lightyagami81161@gmail.com>
2023-06-27 21:55:06 +0530

    4 commit

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const userRouter = require("./routes/user");
 const chatRouter = require("./routes/chats");
 const messageRouter = require("./routes/messages");
